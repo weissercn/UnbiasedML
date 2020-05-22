fixed issues with new analytical derivatives and started tests on W_QCD datasets

UnbiasedML/models.py
@@ -91,43 +91,30 @@ class Classifier(nn.Module):
         if valdataset:
             validation_generator = DataLoader(valdataset,batch_size=len(valdataset),shuffle=False)
         training_generator = DataLoader(traindataset, batch_size=batch_size, shuffle=shuffle,num_workers=num_workers,drop_last=drop_last)
-        t0 = time()        
+        t0 = time()       
+        loss = 0
+        acc = 0
         print("Entering Training...")
         for epoch in range(1,epochs+1):
-            for x,y,m in training_generator:
-                if device!='cpu':
-                    x,y,m = x.to(device),y.to(device),m.to(device)
-                self.train()
-                self.yhat = self(x).view(-1)
-                if epoch<delay_loss:
-                    l = torch.nn.MSELoss()(self.yhat,y)
-                elif pass_x_biased==False:
-                    l = self.loss(pred=self.yhat,target=y)
-                else:
-                    l = self.loss(pred=self.yhat,target=y,x_biased=m)
-                l.backward()
-                self.optimizer.step()
-                self.optimizer.zero_grad()
-                if scheduler:
-                    scheduler.step()
-
-
         #Validation and Printing
             if valdataset:
-                if epoch % interval ==0 or epoch == epochs:
+                if epoch % interval ==0 or epoch == epochs or epoch==1:
                     self.train(False)
                     for x,yval,m in validation_generator:
+                        if device!='cpu':
+                            x,yval,m = x.to(device),yval.to(device),m.to(device)
                         self.yhat_val = self(x).view(-1)
                     valloss = WeightedMSE(yval)
                     l_val = valloss(self.yhat_val,yval)
-                    metrics[0].calculate(pred=self.yhat,target=y,l=l.item())
+                    if epoch != 1:
+                        metrics[0].calculate(pred=self.yhat,target=y,l=l.item())
+                        acc = metrics[0].accs[-1]
                     metrics[1].calculate(pred=self.yhat_val,target=yval,l=l_val.item(),m=m)
                     R50 = metrics[1].R50[-1]
                     JSD = metrics[1].JSD[-1]
                     acc_val = metrics[1].accs[-1]
-                    acc = metrics[0].accs[-1]
                     entry = 'Epoch:{:04d}/{:04d}  ({t:<5.1f}s)\n Train: loss:{:.4f}, acc:{:.0f}% || Val: loss: {:.4f}, acc:{:.0f}%, R50: {:.4f}, 1/JSD: {:.4f}'.format(
-                epoch,epochs,l.item(), 100.* acc,
+                epoch,epochs,loss, 100.* acc,
                 l_val.item(), 100.* acc_val,R50,1/JSD,t=time()-t0)
                     print(entry)
                     if log is not None:
@@ -140,6 +127,24 @@ class Classifier(nn.Module):
                     print(entry)
                     if log is not None:
                         log.entry(entry)
+           # Feed forward 
+            for x,y,m in training_generator:
+                if device!='cpu':
+                    x,y,m = x.to(device),y.to(device),m.to(device)
+                self.train()
+                self.yhat = self(x).view(-1)
+                if epoch<delay_loss:
+                    l = torch.nn.MSELoss()(self.yhat,y)
+                elif pass_x_biased==False:
+                    l = self.loss(pred=self.yhat,target=y)
+                else:
+                    l = self.loss(pred=self.yhat,target=y,x_biased=m)
+                l.backward()
+                loss = l.item()
+                self.optimizer.step()
+                self.optimizer.zero_grad()
+                if scheduler:
+                    scheduler.step()
         if log is not None:
             log.finished()
 
@@ -242,13 +247,14 @@ class FlatLoss():
                     target = target[:-mod] 
             #testing
             mbins = self.bins
-            m, morder  = x_biased.sort()
-            m = m.view(mbins,-1)
-            dm = m[:,-1] - m[:,0]
-            m = m.mean(axis=1)
-            pred = pred[morder].view(mbins,-1).sort(axis=1)[0]
-            fitter = LegendreFitter(m=m, dm=dm,power=2) 
-            LLoss = LegendreIntegral.apply(pred, fitter,None, self.sbins)
+#            m, morder  = x_biased.sort()
+#            m = m.view(mbins,-1)
+#            dm = m[:,-1] - m[:,0]
+#            m = m.mean(axis=1)
+#            pred = pred[morder].view(mbins,-1).sort(axis=1)[0]
+            pred = pred.view(mbins,-1).sort(axis=1)[0]
+            fitter = LegendreFitter(m=x_biased.view(mbins,-1), power=2) 
+            LLoss = LegendreIntegral.apply(pred, fitter, self.sbins)
             ##
             #LLoss = LegendreLoss(x_biased,bins=self.bins,sbins=self.sbins,order=self.order,norm=self.norm)
             #return self.frac*LLoss(pred=pred,target=target) + mse

UnbiasedML/utils.py
@@ -21,6 +21,12 @@ def expand_dims_as(t1,t2):
     result = t1[(...,)+(None,)*t2.dim()] 
     return result 
 
+def Heaviside(tensor):
+    tensor[tensor>0] = 1
+    tensor[tensor==0] = 0.5
+    tensor[tensor<0] = 0
+    return tensor
+
 class LegendreFitter():
     def __init__(self,mbins=None,m=None,order=0,power=1):
         """
@@ -74,13 +80,6 @@ class LegendreFitter():
             fit = fit+ a2*p2
         return fit
 
-
-def Heaviside(tensor):
-    tensor[tensor>0] = 1
-    tensor[tensor==0] = 0.5
-    tensor[tensor<0] = 0
-    return tensor
-
 class LegendreIntegral(Function): 
     @staticmethod
     def forward(ctx, input, fitter,sbins=None):
@@ -96,15 +95,14 @@ class LegendreIntegral(Function):
         sbins : int
             Number of s bins to use in the integral.
         """
-        s_edges = torch.linspace(0,1,sbins+1,dtype=torch.double) #create s edges to integrate over
+        s_edges = torch.linspace(0,1,sbins+1,dtype=torch.double).to(input.device) #create s edges to integrate over
         s = (s_edges[1:] + s_edges[:-1])*0.5
         s = expand_dims_as(s,input)
         ds = s_edges[1:] - s_edges[:-1]
 
         F = Heaviside(s-input).sum(axis=-1).double()/input.shape[-1] # get CDF at s from input values
         integral = (ds.matmul((F-fitter(F))**fitter.power)).sum(axis=0)/input.shape[0]
-        F_state.set(F) # save for plotting
-        s_state.set(s)
+        
         F_s_i =  expand_dims_as(input.view(-1),input) #make a flat copy of input and add dimensions for boradcasting
         F_s_i =  Heaviside(F_s_i-input).sum(axis=-1).double()/input.shape[-1] #sum over bin content to get CDF
         residual = F_s_i - fitter(F_s_i)
@@ -254,13 +252,13 @@ class Metrics():
             R50 = 1/((preds[targets==1]<c).sum()/(targets==1).sum())
             self.R50.append(R50)
             if m is not None:
-                m = np.array(m.tolist()).flatten()
-                p, bins = np.histogram(m[targets==1],bins=50,density = True)
+                m = np.array(m.tolist()).flatten()*250
+                p, bins = np.histogram(m[targets==1],bins=20,density = True)
                 q, _ = np.histogram(m[(targets==1)&(preds<c)],bins=bins,density = True)
                 goodidx = (p!=0)&(q!=0)
                 p = p[goodidx]
                 q = q[goodidx]
-                JSD = np.sum(.5*(p*np.log2(p)+q*np.log2(q)-(p+q)*np.log2((p+q)*0.5)))*(bins[1]-bins[0])
+                JSD = np.sum(.5*(p*np.log2(p)+q*np.log2(q)-(p+q)*np.log2((p+q)*0.5)))#*(bins[1]-bins[0])
                 self.JSD.append(JSD)
         self.accs.append(acc)
         self.signalE.append(signal_efficiency)